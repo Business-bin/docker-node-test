2942a7af00b314ae1559bf134851605b79529d26
SungBin_ <bsb4011@naver.com>
2021-10-16 01:11:59 +0900

    몽고디비 레플리카셋-001

---
+++ docker-mongo/replicaSet.js
@@ -0,0 +1,13 @@
+// replica set 설정
+config = {
+    _id : "replicaSet",
+    members: [
+        {_id:0,host : "mongors1:27017"},
+        {_id:1,host : "mongors2:27017"},
+        {_id:2,host : "mongors3:27017"},
+    ]
+}
+
+rs.initiate(config);
+
+rs.conf();
---
+++ docker-mongo/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sleep 10 | echo Sleeping
+
+mongo mongodb://localhost:27017 /db/configs/replicaSet.js
