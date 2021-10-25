ffa9859af321dc44734f023d697f6e71f1b29f37
SungBin_ <bsb4011@naver.com>
2021-10-25 18:30:29 +0900

    nginx reverse proxy, load balancing

--- routers/api/user.js
+++ routers/api/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router();
-const User = require('../../db/models/user')
-const jwt = require('../../common/jwtToken')
-const crypto = require('../../common/crypto')
-const date = require('../../common/dateFomat')
+const router = express.Router();;
+const User = require('../../db/models/user');
+const jwt = require('../../common/jwtToken');
+const crypto = require('../../common/crypto');
+const date = require('../../common/dateFomat');
 
 router.post('/signup', async (req, res) => {
     try {
@@ -37,10 +37,12 @@ router.post('/signin', async(req, res) => {
         const user = await User.findOne({user_id},{"user_id":true, "user_pw":true, "salt":true}).exec();
         const enPw = await crypto.confirmPw(user_pw, user.salt);
         if(user.user_pw === enPw){
+            console.log(req.rawHeaders)
             const token = jwt.generateToken({});
             res.cookie('access_token', token, {httpOnly:true, maxAge: 1000 * 60 * 60 * 24})
-            // res.send('로그인 성공 '+user.user_id+'님 환영합니다');
-            res.send(`로그인 성공 ${user.user_id}님 환영합니다`);
+            res.send(`로그인 성공 ${user.user_id}님 환영합니다.
+            address : ${req.rawHeaders[11]}`);
+
         }else{
             res.send('로그인 실패! id, password 확인');
         }
