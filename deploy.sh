cf84ffc760ee7778c06114acca591b35a925d4cb
SungBin_ <bsb4011@naver.com>
2021-10-28 19:50:51 +0900

    젠킨스 자동배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo ========================== nodejs deploy shell script start ==========================
 
-
+echo git push
 echo ========================== nodejs deploy shell script end ==========================
