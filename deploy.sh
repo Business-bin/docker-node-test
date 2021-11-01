88b2486681d69b9af1fc21fee987bab9d1ee1751
SungBin_ <bsb4011@naver.com>
2021-11-01 15:03:20 +0900

    젠킨스 자동배포 테스트 006

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo git push test002
 docker rm -f docker-node1
 docker rmi docker-node-test_node1
 docker build -t docker-node-test_node1 .
-docker run -d -p 4997:5000 docker-node1
+docker run -d --name docker-node1 -p 4997:5000 docker-node-test_node1
 echo ========================== nodejs deploy shell script end ==========================
