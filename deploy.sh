b5843a57b44b17c07924b7a8556ec4322218cdb0
SungBin_ <bsb4011@naver.com>
2021-11-01 18:29:03 +0900

    젠킨스 자동배포 테스트 009

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 echo ========================== nodejs deploy shell script start ==========================
 
-echo git push test002
+docker build --pull=true -t docker-node-test_node1 .
+
+echo docker-node1 run!!!!!!!!!!!!!!!!!!!!!
 docker rm -f docker-node1
-docker rmi docker-node-test_node1
-docker build -t docker-node-test_node1 .
 docker run -d --name docker-node1 --network docker-node-test_mongo-networks -p 4997:5000 docker-node-test_node1
 
+echo docker-node2 run!!!!!!!!!!!!!!!!!!!!!
 docker rm -f docker-node2
-docker rmi docker-node-test_node2
-docker build -t docker-node-test_node2 .
 docker run -d --name docker-node2 --network docker-node-test_mongo-networks -p 4998:5000 docker-node-test_node2
 
+echo docker-node3 run!!!!!!!!!!!!!!!!!!!!!
 docker rm -f docker-node3
-docker rmi docker-node-test_node3
-docker build -t docker-node-test_node3 .
 docker run -d --name docker-node3 --network docker-node-test_mongo-networks -p 4999:5000 docker-node-test_node3
+
 echo ========================== nodejs deploy shell script end ==========================
